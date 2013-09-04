90210dc768453682a4119ebd671517cd4fc5cc27
pradine <pradine@uk.ibm.com>
2013-09-04 22:35:23 +0100

    Change the name to IBM WebSphere Application Server Liberty Buildpack

--- config/logging.yml
+++ config/logging.yml
@@ -1,4 +1,4 @@
-# IBM Liberty Buildpack
+# IBM WebSphere Application Server Liberty Buildpack
 # Copyright (c) 2013 the original author or authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- spec/liberty_buildpack/jre/memory/memory_bucket_spec.rb
+++ spec/liberty_buildpack/jre/memory/memory_bucket_spec.rb
@@ -1,5 +1,5 @@
 # Encoding: utf-8
-# IBM Liberty Buildpack
+# IBM WebSphere Application Server Liberty Buildpack
 # Copyright 2013 the original author or authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
