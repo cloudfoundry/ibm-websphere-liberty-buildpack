993d6f8186113b5ced2d76392cc84ae1c729a02f
samiamad <samiamad@uk.ibm.com>
2013-10-24 16:27:25 +0100

    edited .yml files and reverted all changes to original.

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -47,24 +47,14 @@ module LibertyBuildpack::Container
     #
     # @return [Array<String>] :array of file names of discovered applications
     def apps
-      p "apps method"
       apps_found = []
       server_xml = Liberty.server_xml(@app_dir)
       if Liberty.web_inf(@app_dir)
-        p "web-inf first case"
         apps_found = [@app_dir]
         
-      elsif apps_found = Dir.glob('*.ear') #Liberty.meta_inf(@app_dir) #this is never going to be the case since .ears are not expanded by cf
-        p "second case .ear exists"
-        Liberty.expand_apps(apps_found)
-        
       elsif server_xml
-        p "third case server.xml"
-        #apps_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', '*.war')))
-        wars_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', '*.war')))
+        apps_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', '*.war')))
         # searches for files that satisfy server.xml/../**/*.war and returns an array of the matches
-        ears_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', '*.ear'))) 
-        apps_found = wars_found << ears_found
         Liberty.expand_apps(apps_found)
       end
       apps_found
