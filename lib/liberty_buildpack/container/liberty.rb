a1418fc711342600b9c7621d5ee862f61f808965
samiamad <samiamad@uk.ibm.com>
2013-10-25 09:26:55 +0100

    commented out all changes since psych syntax error stuck

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -208,11 +208,11 @@ module LibertyBuildpack::Container
     # first checkpoint for .ears and other applications
     def self.find_liberty(app_dir, configuration)
       puts "[INFO] find liberty directory: #{app_dir} config: #{configuration}"
-      if Liberty.ear(app_dir)
-        version, uri = LibertyBuildpack::Repository::ConfiguredItem.find_item(configuration) do |candidate_version|
-          fail "Malformed Liberty version #{candidate_version}: too many version components" if candidate_version[4]
-        end
-      elsif server_xml(app_dir)
+      #if Liberty.ear(app_dir)
+       # version, uri = LibertyBuildpack::Repository::ConfiguredItem.find_item(configuration) do |candidate_version|
+        #  fail "Malformed Liberty version #{candidate_version}: too many version components" if candidate_version[4]
+        #end
+      if server_xml(app_dir)
         version, uri = LibertyBuildpack::Repository::ConfiguredItem.find_item(configuration) do |candidate_version|
           fail "Malformed Liberty version #{candidate_version}: too many version components" if candidate_version[4]
         end
