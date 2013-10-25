6255755c7ed7f58196f22ebc3f65a764098d8c88
chibi03 <saa175@bham.ac.uk>
2013-10-25 10:33:06 +0100

    one of the prints in server name causes the error, testing one by one which. 3

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -169,10 +169,10 @@ module LibertyBuildpack::Container
         raise "Incorrect number of servers to deploy (expecting exactly one): #{candidates}" if candidates.size != 1
         File.basename(candidates[0])
       elsif Liberty.server_directory @app_dir
-        #puts "[INFO] pushed unpackaged server"
+        puts "[INFO] pushed unpackaged server"
         return 'defaultServer'
       elsif Liberty.web_inf @app_dir
-        #puts "[INFO] pushed plain application (web-inf)"
+        puts "[INFO] pushed plain application (web-inf)"
         return 'defaultServer'
       #elsif Liberty.ear(@app_dir) #this causes the weird error to occur!!
         #puts "[INFO] pushed ear"
