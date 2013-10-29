c1f81f15df5cff967b2666b2a36a9c7c6ceb6030
chibi03 <saa175@bham.ac.uk>
2013-10-29 10:08:59 +0000

    do nothing with .ear

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -54,7 +54,7 @@ module LibertyBuildpack::Container
       
       elsif Liberty.ear(@app_dir)  
         apps_found = Dir.glob(File.expand_path(File.join(@app_dir, '*.ear')))
-        Liberty.expand_ear(apps_found)
+        # Liberty.expand_ear(apps_found)
       elsif server_xml
         apps_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', ['*.war', '*.ear'])))
         # searches for files that satisfy server.xml/../**/*.war and returns an array of the matches
