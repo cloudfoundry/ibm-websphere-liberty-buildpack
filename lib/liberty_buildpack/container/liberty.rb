c41332af47a2280f7fd7993b41a76a8f406d7909
chibi03 <saa175@bham.ac.uk>
2013-10-31 16:30:45 +0000

    print to debug

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -58,8 +58,8 @@ module LibertyBuildpack::Container
         apps_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', ['*.war', '*.ear']))) # searches for files that satisfy server.xml/../**/*.war and returns an array of the matches
       end
       
-      puts "current status =#{@status}"
       if @status == :detect
+        puts "first extraction"
         if Liberty.contains_ear(@app_dir)  
           ear_found = Dir.glob(File.expand_path(File.join(@app_dir, '*.ear')))
           Liberty.expand_ear(ear_found)
