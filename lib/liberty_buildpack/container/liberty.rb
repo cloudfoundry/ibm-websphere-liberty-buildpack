93ec1c319ce983aa94214679570caad9a40410e8
chibi03 <saa175@bham.ac.uk>
2013-11-05 14:11:51 +0000

    only find .wars

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -65,7 +65,7 @@ module LibertyBuildpack::Container
         @logger.info("single ear push, path to ear: #{apps_found}")
       elsif server_xml
         @logger.info("Determined a server push, searching for apps and extracting.")
-        apps_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', ['*.war', '*.ear']))) # searches for files that satisfy server.xml/../**/*.war and returns an array of the matches
+        apps_found = Dir.glob(File.expand_path(File.join(server_xml, '..', '**', '*.war' ))) # searches for files that satisfy server.xml/../**/*.war and returns an array of the matches
         @logger.info("Expanded apps, path to apps: #{apps_found}")
         if !Liberty.all_extracted?(apps_found) 
           Liberty.expand_apps(apps_found)
