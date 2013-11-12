d0eae31de37cd3de4e1bb86686f4be890a1c8356
chibi03 <saa175@bham.ac.uk>
2013-11-12 13:51:22 +0000

    added logging to determine behaviour

--- lib/liberty_buildpack/framework/framework_utils.rb
+++ lib/liberty_buildpack/framework/framework_utils.rb
@@ -59,20 +59,25 @@ module LibertyBuildpack::Framework
 
     def self.link_libs(apps, lib_dir)
       apps.each do |app_dir|
-        libs = LibertyBuildpack::Container::ContainerUtils.libs(app_dir, lib_dir) # Returns an +Array+ containing the relative paths
-        # of the JARs located in the additional libraries directory.
+        libs = LibertyBuildpack::Container::ContainerUtils.libs(app_dir, lib_dir) 
+        LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("Current app #{app_dir}")
         if libs
+          LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("libs is not nli #{libs}")
           if LibertyBuildpack::Container::Liberty.web_inf(app_dir)
             app_web_inf_lib = web_inf_lib(app_dir)
             FileUtils.mkdir_p(app_web_inf_lib) unless File.exists?(app_web_inf_lib)
             app_web_inf_lib_path = Pathname.new(app_web_inf_lib)
+            LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("A war was found and the lib is being linked")
             Pathname.glob(File.join(lib_dir, '*.jar')) do |jar|
+              LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("linking #{jar} to #{app_web_inf_lib}")
               FileUtils.ln_sf(jar.relative_path_from(app_web_inf_lib_path), app_web_inf_lib)
             end
           elsif LibertyBuildpack::Container::Liberty.meta_inf(app_dir)
             ear_lib_path = Pathname.new(ear_lib)
             FileUtils.mkdir_p(ear_lib) unless File.exists?(ear_lib)
+            LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("A ear was found and the lib is being linked")
             Pathname.glob(File.join(lib_dir, '*.jar')) do |jar|
+              LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("linking #{jar} to #{ear_lib}")
               FileUtils.ln_sf(jar.relative_path_from(ear_lib_path), ear_lib)
             end
           end
