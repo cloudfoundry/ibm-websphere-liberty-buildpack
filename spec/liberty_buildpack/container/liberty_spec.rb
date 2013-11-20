458f17f3ec520c2ee9a5c7d1a28af0606152199d
chibi03 <saa175@bham.ac.uk>
2013-11-20 13:10:31 +0000

    Fix server.xml update test doesn't include type=app_type - a \n seperates the type value and the rest making separate include necessary

--- spec/liberty_buildpack/container/liberty_spec.rb
+++ spec/liberty_buildpack/container/liberty_spec.rb
@@ -320,7 +320,7 @@ module LibertyBuildpack::Container
           server_xml_contents = File.read(server_xml_file)
           expect(server_xml_contents.include? '<featureManager>').to be_true
           expect(server_xml_contents.include? '<application name="myapp" context-root="/" location="../../../../../"').to be_true
-          expect(server_xml_contents.include? 'type=\'war\'').to be_true
+          expect(server_xml_contents.include? 'type="war"').to be_true
           expect(server_xml_contents.include? 'httpPort="${port}"').to be_true
         end
       end
@@ -351,7 +351,7 @@ module LibertyBuildpack::Container
           server_xml_contents = File.read(server_xml_file)
           expect(server_xml_contents.include? '<featureManager>').to be_true
           expect(server_xml_contents.include? '<application name="myapp" context-root="/" location="../../../../../"').to be_true
-          expect(server_xml_contents.include? 'type=\'ear\'').to be_true
+          expect(server_xml_contents.include? 'type="ear"').to be_true
           expect(server_xml_contents.include? 'httpPort="${port}"').to be_true
         end
       end
