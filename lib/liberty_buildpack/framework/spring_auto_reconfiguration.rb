6ada2ccf6e0c12183df2b01bf28d955e233cb837
chibi03 <saa175@bham.ac.uk>
2013-11-22 15:29:52 +0000

    Debugging - ear fails to stage - understanding error in find spring

--- lib/liberty_buildpack/framework/spring_auto_reconfiguration.rb
+++ lib/liberty_buildpack/framework/spring_auto_reconfiguration.rb
@@ -113,10 +113,9 @@ module LibertyBuildpack::Framework
       end
 
       def self.spring_application?(app_dir, lib_dir)
-        # LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("Checking for Spring app in app: #{app_dir} & lib #{lib_dir}")
-        # spring_apps = FrameworkUtils.find(app_dir, SPRING_JAR_PATTERN)
-        # (spring_apps != nil && spring_apps != []) || FrameworkUtils.application_within_archive?(app_dir, 'spring-core')
-        return false
+        spring_apps = FrameworkUtils.find(app_dir, SPRING_JAR_PATTERN)
+        LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info("Checking for Spring app in app: #{app_dir} & lib #{lib_dir}")
+        (spring_apps != nil && spring_apps != []) || FrameworkUtils.application_within_archive?(app_dir, 'spring-core')
       end
   end
 
