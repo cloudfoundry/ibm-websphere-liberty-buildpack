ed0c55cd0df10de90cf2c6135bf016b173f37b55
chibi03 <saa175@bham.ac.uk>
2013-11-22 16:22:07 +0000

    Debugging - ear fails to stage - missing end

--- spec/liberty_buildpack/framework/spring_auto_reconfiguration_spec.rb
+++ spec/liberty_buildpack/framework/spring_auto_reconfiguration_spec.rb
@@ -71,7 +71,7 @@ module LibertyBuildpack::Framework
 
         LibertyBuildpack::Repository::ConfiguredItem.stub(:find_item).and_return(SPRING_AUTO_RECONFIGURATION_DETAILS)
         LibertyBuildpack::Util::ApplicationCache.stub(:new).and_return(application_cache)
-        application_cache.stub(:get).with('test-uri').and_yield(File.open('spec/fixtures/stub-auto-reconfiguration.jar'))
+        application_cache.stub(:get).with('blah').and_yield(File.open('spec/fixtures/stub-auto-reconfiguration.jar'))
 
         SpringAutoReconfiguration.new(
           app_dir: root,
