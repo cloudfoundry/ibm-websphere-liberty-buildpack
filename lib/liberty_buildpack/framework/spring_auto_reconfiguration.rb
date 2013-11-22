005ecffedf84f61296b8768a225d7b383aad65a0
chibi03 <saa175@bham.ac.uk>
2013-11-22 15:05:45 +0000

    Debugging

--- lib/liberty_buildpack/framework/spring_auto_reconfiguration.rb
+++ lib/liberty_buildpack/framework/spring_auto_reconfiguration.rb
@@ -109,6 +109,7 @@ module LibertyBuildpack::Framework
       end
 
       def self.spring_application?(app_dir, lib_dir)
+        LibertyBuildpack::Diagnostics::LoggerFactory.get_logger.info('Checking for Spring app')
         spring_apps = FrameworkUtils.find(app_dir, SPRING_JAR_PATTERN)
         (spring_apps != nil && spring_apps != []) || FrameworkUtils.application_within_archive?(app_dir, 'spring-core')
       end
