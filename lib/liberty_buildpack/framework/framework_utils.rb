673f9f61fe1a189d1daeb3169274d26a5c913a8e
chibi03 <saa175@bham.ac.uk>
2013-11-25 13:15:30 +0000

    ears fail to stage again..

--- lib/liberty_buildpack/framework/framework_utils.rb
+++ lib/liberty_buildpack/framework/framework_utils.rb
@@ -46,16 +46,16 @@ module LibertyBuildpack::Framework
     end
 
     def self.application_within_archive?(app_dir, pattern)
-      list = ''
-      archives = Dir.glob(File.join(app_dir, '**', '*.jar'))
-      archives.each do |file|
-        IO.popen("unzip -l -qq #{file}") do |io| 
-          while (line = io.gets)
-            list << "#{line}"
-          end
-        end
-      end
-      list.include? pattern
+      #list = ''
+      #archives = Dir.glob(File.join(app_dir, '**', '*.jar'))
+      #archives.each do |file|
+        #IO.popen("unzip -l -qq #{file}") do |io| 
+         # while (line = io.gets)
+          #  list << "#{line}"
+         # end
+       # end
+     # end
+      #list.include? pattern
     end
 
     def self.create_lib_dir(start_dir)
