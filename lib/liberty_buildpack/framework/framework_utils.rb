21d6a7e911e3af92e93972d26c58f7526136edad
chibi03 <saa175@bham.ac.uk>
2013-11-28 10:37:19 +0000

    Rubocop compilant formatting part 3

--- lib/liberty_buildpack/framework/framework_utils.rb
+++ lib/liberty_buildpack/framework/framework_utils.rb
@@ -54,7 +54,8 @@ module LibertyBuildpack::Framework
       archives = Dir.glob(File.join(app_dir, '**', '*.jar'))
       archives.each do |file|
         IO.popen("unzip -l -qq #{file}") do |io|
-          list << "#{line}" while (line = io.gets)
+          line = io.gets
+          list << "#{line}" while line
         end
       end
       list.include? pattern
