0cc212b18ecb3b20ff36b5ff14b8de3b9008e6db
Jarek Gawor <jgawor@gmail.com>
2014-06-05 23:54:31 -0400

    autoconfiguration for mysql and postgresql databases

---
+++ lib/liberty_buildpack/util/heroku.rb
@@ -0,0 +1,119 @@
+# Encoding: utf-8
+# IBM WebSphere Application Server Liberty Buildpack
+# Copyright (c) 2014 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'liberty_buildpack/util'
+
+module LibertyBuildpack::Util
+
+  # Heroku utilities.
+  class Heroku
+
+    # Detects Heroku environment.
+    #
+    # @return [Boolean] +true+ if running on Heroku. +False+ otherwise.
+    def self.heroku?
+      ENV['DYNO'].nil? ? false : true
+    end
+
+    # Generates VCAP_SERVICES style map from a map that represents the environment variables
+    # as set on Heroku. Each bound service on Heroku sets one or more environment variables that
+    # specifies the location (in the form of a URL) of the service.
+    #
+    # @param [Hash] env - the map with environment variables
+    # @return [Hash] VCAP_SERVICES style map
+    def generate_vcap_services(env)
+      vcap_services = {}
+      service_name_map = parse_service_name_map(env)
+      env.each do | key, value |
+        if key.end_with?(URL_SUFFIX)
+          begin
+            uri = URI.parse(value)
+          rescue URI::InvalidURIError
+            next
+          end
+
+          if key.start_with?(POSTGRESQL_PREFIX)
+            type, service = handle_postgresql(key, uri, service_name_map)
+          elsif key.start_with?(CLEARDB_PREFIX)
+            type, service = handle_cleardb(key, uri, service_name_map)
+          else
+            type = key
+            service = {}
+            service['name'] = service_name_map[key] || generate_name(key)
+            service['credentials'] = create_default_credentials(uri)
+          end
+
+          vcap_services[type] = [service]
+        end
+      end
+      vcap_services
+    end
+
+    private
+
+    URL_SUFFIX = '_URL'.freeze
+    POSTGRESQL_PREFIX = 'HEROKU_POSTGRESQL_'.freeze
+    CLEARDB_PREFIX = 'CLEARDB_DATABASE_'.freeze
+
+    def handle_postgresql(key, uri, service_name_map)
+      type = 'postgresql'
+      service = {}
+      service['name'] = service_name_map[key] || type + '.' + key[POSTGRESQL_PREFIX.size..-URL_SUFFIX.size - 1].downcase
+      service['tags'] = ['postgresql']
+      service['credentials'] = create_default_credentials(uri)
+      [key, service]
+    end
+
+    def handle_cleardb(key, uri, service_name_map)
+      type = 'cleardb'
+      service = {}
+      service['name'] = service_name_map[key] || type
+      service['tags'] = ['mysql']
+      service['credentials'] = create_default_credentials(uri)
+      [key, service]
+    end
+
+    def create_default_credentials(uri)
+      credentials = {}
+      credentials['host'] = credentials['hostname'] = uri.host
+      credentials['port'] = uri.port unless uri.port.nil?
+      credentials['user'] = credentials['username'] = uri.user unless uri.user.nil?
+      credentials['password'] = uri.password unless uri.password.nil?
+      credentials['name'] = uri.path[1..-1]  unless uri.path[1..-1].nil?
+      credentials['uri'] = credentials['url'] = uri.to_s
+      credentials
+    end
+
+    def generate_name(url)
+      # remove _URL at the end & downcase it
+      name = url[0..-URL_SUFFIX.size - 1].downcase
+      name
+    end
+
+    def parse_service_name_map(env)
+      name_map = env['SERVICE_NAME_MAP']
+      map = {}
+      unless name_map.nil?
+        name_map.split(';').each do | value |
+          key_value = value.split('=')
+          map[key_value[0].strip] = key_value[1].strip if key_value.size == 2
+        end
+      end
+      map
+    end
+  end
+
+end
