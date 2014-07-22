e92c5a63f9d322fe5266494ba5daa2725a572d59
Sinee Paungam <spaungam@us.ibm.com>
2014-07-21 21:29:14 -0500

    issue 102: no update check required for missing etag

--- lib/liberty_buildpack/util/cache/download_cache.rb
+++ lib/liberty_buildpack/util/cache/download_cache.rb
@@ -221,7 +221,7 @@ module LibertyBuildpack::Util::Cache
       @logger.debug { "should_use_cache for #{uri}, inputs: use_internet? = #{use_internet}, cached? = #{cached}, has_etag? = #{has_etag}, has_last_modified? = #{has_last_modified}" }
 
       use_cache = false
-      if cached && !has_etag && !has_last_modified
+      if cached && (!has_etag || !has_last_modified)
         @logger.debug { "Using cache version of #{uri} without up-to-date check since it has no etag or last modified timestamp" }
         use_cache = true
       elsif use_internet && cached && (has_etag || has_last_modified)
