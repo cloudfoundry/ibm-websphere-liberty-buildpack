00ef26e2835662aa0cff4a859884ff9bbed6775b
ThomWright <github@thomwright.co.uk>
2014-09-04 13:06:53 +0000

    Use different User-Agent in RSpec HTTP requests
    RSpecs that send actual HTTP requests to download assets should
    use a non-default User-Agent to make it clear the requests are
    coming from tests.

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,11 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update({ 'IBM_JVM_LICENSE' => 'L-AWON-8GALN9', 'IBM_LIBERTY_LICENSE' => 'L-JTHS-95XRL8' })
+    ENV.update({ 'IBM_JVM_LICENSE' => 'L-AWON-8GALN9', 'IBM_LIBERTY_LICENSE' => 'L-JTHS-95XRL8', 'USER_AGENT' => 'RSpec-Test' })
+  end
+
+  after(:each) do
+    ENV.delete('USER_AGENT')
   end
 
   after(:all) do
