6b59a6ae4ef45603b48e6f567ee323bf6542e256
Sinee Paungam <spaungam@us.ibm.com>
2015-01-09 14:35:39 -0600

    support for user-provided new relic service

---
+++ lib/liberty_buildpack/services/vcap_services.rb
@@ -0,0 +1,80 @@
+# Encoding: utf-8
+# IBM WebSphere Application Server Liberty Buildpack
+# Copyright 2013-2015 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'liberty_buildpack/services'
+
+# A module encapsulating all of the services components for the Liberty buildpack
+module LibertyBuildpack::Services
+
+  # An abstraction encapsulating the +VCAP_SERVICES+ of an application.
+  #
+  # A new instance of this type should be created once for the application.
+  class VcapServices < Array
+
+    def initialize(raw)
+      @logger = LibertyBuildpack::Diagnostics::LoggerFactory.get_logger
+      concat raw.values.flatten
+    end
+
+    # Compares the name, label, and tags of each service to the given +filter+.  The method returns +true+ if the
+    # +filter+ matches exactly one service, +false+ otherwise.
+    #
+    # @param [Regexp, String] filter a +RegExp+ or +String+ to match against the name, label, and tags of the services
+    # @param [String] required_credentials an optional list of keys or groups of keys, where at one key from the
+    #                                      group, must exist in the credentials payload of the candidate service
+    # @return [Boolean] +true+ if the +filter+ matches exactly one service with the required credentials, +false+
+    #                   otherwise.
+    def one_service?(filter, *required_credentials)
+      candidates = select(&matcher(filter))
+      match = false
+      if candidates.one?
+        if credentials?(candidates.first['credentials'], required_credentials)
+          match = true
+        else
+          @logger.warn("A service with a name label or tag matching #{filter} was found, but was missing one of the required credentials #{required_credentials}")
+        end
+      end
+
+      match
+    end
+
+    # Compares the name, label, and tags of each service to the given +filter+.  The method returns the first service
+    # that the +filter+ matches.  If no service matches, returns +nil+
+    #
+    # @param [Regexp, String] filter a +RegExp+ or +String+ to match against the name, label, and tags of the services
+    # @return [Hash, nil] the first service that +filter+ matches.  If no service matches, returns +nil+.
+    def find_service(filter)
+      find(&matcher(filter))
+    end
+
+    private
+
+    def credentials?(candidate, required_keys)
+      required_keys.all? do |k|
+        k.is_a?(Array) ? k.any? { |g| candidate.key?(g) } : candidate.key?(k)
+      end
+    end
+
+    def matcher(filter)
+      filter = Regexp.new(filter) unless filter.is_a?(Regexp)
+
+      lambda do |service|
+        (service['label'] == 'user-provided' && service['name'] =~ filter) || service['label'] =~ filter || !service['tags'].nil? && service['tags'].any? { |tag| tag =~ filter }
+      end
+    end
+  end
+end
+
---
+++ spec/liberty_buildpack/services/vcap_services_spec.rb
@@ -0,0 +1,130 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2015 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+require 'logging_helper'
+require 'liberty_buildpack/services/vcap_services'
+
+describe LibertyBuildpack::Services::VcapServices do
+  include_context 'logging_helper'
+
+  shared_context 'service context' do
+    let(:service) do
+      { 'name' => 'test-name', 'label' => label, 'tags' => ['test-tag'], 'plan' => 'test-plan',
+      'credentials' => { 'uri' => 'test-uri', 'h1' => 'foo', 'h2' => 'foo' } }
+    end
+
+    let(:services) { described_class.new('test' => [service]) }
+  end
+
+  shared_examples_for 'a default service with a label value:' do | label_value |
+    include_context 'service context' do
+      let(:label) { label_value }
+    end
+
+    it 'returns false from one_service? if there is no service that matches' do
+      expect(services.one_service? 'bad-test').not_to be
+      expect(services.one_service?(/bad-test/)).not_to be
+    end
+
+    it 'returns true from one_service? if there is a matching tag' do
+      expect(services.one_service? 'test-tag').to be
+      expect(services.one_service?(/test-tag/)).to be
+    end
+
+    it 'returns false from one_service? if there is a matching service without required credentials' do
+      expect(services.one_service? 'test-tag', 'bad-credential').not_to be
+      expect(services.one_service?(/test-tag/, 'bad-credential')).not_to be
+    end
+
+    it 'returns true from one_service? if there is a matching service with required credentials' do
+      expect(services.one_service? 'test-tag', 'uri').to be
+      expect(services.one_service?(/test-tag/, 'uri')).to be
+    end
+
+    it 'returns true from one_service? if there is a matching service with one required group credentials' do
+      expect(services.one_service? 'test-tag', %w(uri other)).to be
+      expect(services.one_service?(/test-tag/, %w(uri other))).to be
+    end
+
+    it 'returns true from one_service? if there is a matching service with two required group credentials' do
+      expect(services.one_service? 'test-tag', %w(h1 h2)).to be
+      expect(services.one_service?(/test-tag/, %w(h1 h2))).to be
+    end
+
+    it 'returns false from one_service? if there is a matching service with no required group credentials' do
+      expect(services.one_service? 'test-tag', %w(foo bar)).not_to be
+      expect(services.one_service?(/test-tag/, %w(foo bar))).not_to be
+    end
+
+    it 'returns nil from find_service? if there is no service that matches' do
+      expect(services.find_service 'bad-test').to be_nil
+      expect(services.find_service(/bad-test/)).to be_nil
+    end
+
+    it 'returns service from find_service? if there is a matching tag' do
+      expect(services.find_service 'test-tag').to be(service)
+      expect(services.find_service(/test-tag/)).to be(service)
+    end
+  end # end of shared example
+
+  describe 'a default service provided by the platform' do
+    it_behaves_like 'a default service with a label value:', 'test-label'
+
+    describe 'match service based on label' do
+      include_context 'service context' do
+        let(:label) { 'test-label' }
+      end
+
+      it 'returns service from find_service? if there is a matching label' do
+        expect(services.find_service 'test-label').to be(service)
+        expect(services.find_service(/test-label/)).to be(service)
+      end
+
+      it 'returns service from find_service? if there is a matching name' do
+        expect(services.find_service 'test-name').to be_nil
+        expect(services.find_service(/test-name/)).to be_nil
+      end
+
+      it 'returns true from one_service? if there is a matching label' do
+        expect(services.one_service? 'test-label').to be
+        expect(services.one_service?(/test-label/)).to be
+      end
+
+      it 'returns true from one_service? if there is a matching name' do
+        expect(services.one_service? 'test-name').not_to be
+        expect(services.one_service?(/test-name/)).not_to be
+      end
+
+    end
+  end
+
+  describe 'a service provided by the user cannot be matched to a label' do
+    it_behaves_like 'a default service with a label value:', 'user-provided'
+
+    describe 'match service based on name' do
+      include_context 'service context' do
+        let(:label) { 'user-provided' }
+      end
+
+      it 'returns service from find_service? if there is a matching name' do
+        expect(services.find_service 'test-name').to be(service)
+        expect(services.find_service(/test-name/)).to be(service)
+      end
+    end
+  end
+
+end
