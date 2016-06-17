8ff8b40739986d355b223687a572f9176cefa1f3
Joseph Marrero <jmarrero@gmail.com>
2016-06-17 16:07:20 -0400

    Port of the container_certificate_trust_store from java buildpack

---
+++ lib/liberty_buildpack/framework/container_certificate_trust_store.rb
@@ -0,0 +1,187 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2016 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'liberty_buildpack/framework'
+require 'liberty_buildpack/util/dash_case'
+require 'liberty_buildpack/util/format_duration'
+require 'fileutils'
+require 'shellwords'
+require 'tempfile'
+
+module LibertyBuildpack::Framework
+
+    # Encapsulates the functionality for contributing container-based certificates to an application.
+    class ContainerCertificateTrustStore
+
+      # Creates an instance, passing in a context of information available to the component
+      #
+      # @param [Hash] context the context that is provided to the instance
+      # @option context [Hash] :configuration the properties provided by the user
+      # @option context [String] :app_dir the directory that the application exists in
+      # @option context [String] :java_home the directory that acts as +JAVA_HOME+
+      # @option context [Array<String>] :java_opts an array that Java options can be added to
+      def initialize(context = {})
+        @configuration = context[:configuration]
+        @app_dir = context[:app_dir]
+        @java_home = context[:java_home]
+        @java_opts = context[:java_opts]
+      end
+
+      # If the component should be used when staging an application
+      #
+      # @return [Array<String>, String, nil] If the component should be used when staging the application, a +String+ or
+      #                                      an +Array<String>+ that uniquely identifies the component (e.g.
+      #                                      +open_jdk=1.7.0_40+).  Otherwise, +nil+.
+      def detect
+        supports_local_certificates ? id(certificates.length) : nil
+      end
+
+      # Modifies the application's file system.  The component is expected to transform the application's file system in
+      # whatever way is necessary (e.g. downloading files or creating symbolic links) to support the function of the
+      # component.  Status output written to +STDOUT+ is expected as part of this invocation.
+      #
+      # @return [Void]
+      def compile
+        puts '-----> Creating TrustStore with container certificates'
+
+        resolved_certificates = certificates
+        with_timing(caption(resolved_certificates)) do
+          unless used_trust_store == 'jvm'
+            FileUtils.mkdir_p @app_dir + '.container_certificate_trust_store'
+          end
+          resolved_certificates.each_with_index { |certificate, index| add_certificate certificate, index }
+        end
+      end
+
+      # Modifies the application's runtime configuration. The component is expected to transform members of the
+      # +context+ # (e.g. +@java_home+, +@java_opts+, etc.) in whatever way is necessary to support the function of the
+      # component.
+      #
+      # Container components are also expected to create the command required to run the application.  These components
+      # are expected to read the +context+ values and take them into account when creating the command.
+      #
+      # @return [void, String] components other than containers and JREs are not expected to return any value.
+      #                        Container and JRE components are expected to return a command required to run the
+      #                        application.
+      def release
+        unless used_trust_store == 'jvm'
+          # Hardcoded truststore location since @app_dir changes from staging to runtime and the java opts are set on staging.
+          @java_opts << '-Djavax.net.ssl.trustStore=/home/vcap/app/.container_certificate_trust_store/truststore.jks'
+          @java_opts << "-Djavax.net.ssl.trustStorePassword=#{password}"
+        end
+      end
+
+      private
+
+      CA_CERTIFICATES = Pathname.new('/etc/ssl/certs/ca-certificates.crt').freeze
+
+      JVM_KEY_STORE = Pathname.new('$JAVA_HOME/jre/lib/security/cacerts').freeze
+
+      LOCAL_CERTS_ENABLED = 'enabled'.freeze
+
+      TRUST_STORE_USED = 'trust_store'.freeze
+
+      private_constant :CA_CERTIFICATES
+
+      # Wrap the execution of a block with timing information
+      #
+      # @param [String] caption the caption to print when timing starts
+      # @return [Void]
+      def with_timing(caption)
+        start_time = Time.now
+        print "#{caption}"
+        yield
+        puts "(#{(Time.now - start_time).duration})"
+      end
+
+      def add_certificate(certificate, index)
+        file = write_certificate certificate
+        shell "#{keytool} -importcert -noprompt -keystore #{trust_store} -storepass #{password} -file #{file.to_path} -alias certificate-#{index}"
+      end
+
+      def ca_certificates
+        CA_CERTIFICATES
+      end
+
+      def caption(resolved_certificates)
+        "Adding #{resolved_certificates.count} certificates to #{trust_store}"
+      end
+
+      def certificates
+        certificates = []
+
+        certificate = nil
+        ca_certificates.each_line do |line|
+          if line =~ /BEGIN CERTIFICATE/
+            certificate = line
+          elsif line =~ /END CERTIFICATE/
+            certificate += line
+            certificates << certificate
+            certificate = nil
+          elsif !certificate.nil?
+            certificate += line
+          end
+        end
+
+        certificates
+      end
+
+      def id(count)
+        "#{self.class.to_s.dash_case}=#{count}"
+      end
+
+      def keytool
+        instance_variable_get('@java_home') + 'bin/keytool'
+      end
+
+      def password
+        'java-buildpack-trust-store-password'
+      end
+
+      def supports_configuration?
+        !@configuration.nil? && @configuration[LOCAL_CERTS_ENABLED]
+      end
+
+      def supports_file?
+        ca_certificates.exist?
+      end
+
+      def supports_local_certificates
+        supports_configuration? && supports_file?
+      end
+
+      def used_trust_store
+        @configuration[TRUST_STORE_USED] unless @configuration.nil?
+      end
+
+      def trust_store
+        if used_trust_store == 'jvm'
+          JVM_KEY_STORE
+        else
+          @app_dir + '.container_certificate_trust_store/truststore.jks'
+        end
+      end
+
+      def write_certificate(certificate)
+        file = Tempfile.new('certificate-')
+        file.write(certificate)
+        file.fsync
+        file
+      end
+
+    end
+
+end
---
+++ spec/liberty_buildpack/framework/container_certificate_trust_store_spec.rb
@@ -0,0 +1,90 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2016 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+require 'component_helper'
+require 'liberty_buildpack/framework/container_certificate_trust_store'
+
+describe LibertyBuildpack::Framework::ContainerCertificateTrustStore do
+  include_context 'component_helper'
+
+  let(:ca_certificates) { Pathname.new('spec/fixtures/ca-certificates.crt') }
+
+  it 'detects with ca-certificates file' , configuration: { 'enabled' => 'true' } do
+    allow(component).to receive(:ca_certificates).and_return(ca_certificates)
+
+    expect(component.detect).to eq('container-certificate-trust-store=3')
+  end
+
+  it 'detects with ca-certificates file' , configuration: { 'enabled' => 'true' } do
+    allow(component).to receive(:ca_certificates).and_return(ca_certificates)
+
+    expect(component.detect).to eq('container-certificate-trust-store=3')
+  end
+
+  it 'does not detect without ca-certificates file' do
+    allow(component).to receive(:ca_certificates).and_return(Pathname.new('spec/fixtures/ca-certificates-no-exist.crt'))
+
+    expect(component.detect).to be_nil
+  end
+
+  it 'does not detect when disabled and trust_store set' , configuration: { 'enabled' => false, 'trust_store' => 'jvm' } do
+    allow(component).to receive(:ca_certificates).and_return(ca_certificates)
+
+    expect(component.detect).to be_nil
+  end
+
+  it 'detects with ca-certificates file when trust_store is set' , configuration: { 'enabled' => true, 'trust_store' => 'jvm' } do
+    allow(component).to receive(:ca_certificates).and_return(ca_certificates)
+
+    expect(component.detect).to eq('container-certificate-trust-store=3')
+  end
+
+  it 'creates truststore' , java_home: '/my/java_home/',  configuration: { 'enabled' => true, 'trust_store' => 'new' } do
+    app_dir = component.instance_variable_get('@app_dir')
+
+    allow(component).to receive(:ca_certificates).and_return(ca_certificates)
+    allow(component).to receive(:write_certificate).and_return(Pathname.new('/certificate-0'),
+                                                               Pathname.new('/certificate-1'),
+                                                               Pathname.new('/certificate-2'))
+    allow(component).to receive(:shell).with("/my/java_home/bin/keytool -importcert -noprompt -keystore #{app_dir}/.container_certificate_trust_store/truststore.jks -storepass java-buildpack-trust-store-password -file /certificate-0 -alias certificate-0")
+    allow(component).to receive(:shell).with("/my/java_home/bin/keytool -importcert -noprompt -keystore #{app_dir}/.container_certificate_trust_store/truststore.jks -storepass java-buildpack-trust-store-password -file /certificate-1 -alias certificate-1")
+    allow(component).to receive(:shell).with("/my/java_home/bin/keytool -importcert -noprompt -keystore #{app_dir}/.container_certificate_trust_store/truststore.jks -storepass java-buildpack-trust-store-password -file /certificate-2 -alias certificate-2")
+
+    component.compile
+  end
+
+  it 'replaces jvm trustStore with system cert' , java_home: '/my/java_home/',  configuration: { 'enabled' => true, 'trust_store' => 'jvm' } do
+    allow(component).to receive(:ca_certificates).and_return(ca_certificates)
+    allow(component).to receive(:write_certificate).and_return(Pathname.new('/certificate-0'),
+                                                               Pathname.new('/certificate-1'),
+                                                               Pathname.new('/certificate-2'))
+    allow(component).to receive(:shell).with('/my/java_home/bin/keytool -importcert -noprompt -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass java-buildpack-trust-store-password -file /certificate-0 -alias certificate-0')
+    allow(component).to receive(:shell).with('/my/java_home/bin/keytool -importcert -noprompt -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass java-buildpack-trust-store-password -file /certificate-1 -alias certificate-1')
+    allow(component).to receive(:shell).with('/my/java_home/bin/keytool -importcert -noprompt -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass java-buildpack-trust-store-password -file /certificate-2 -alias certificate-2')
+
+    component.compile
+  end
+
+  it 'adds truststore properties', java_opts: [] do
+    component.release
+    java_opts = component.instance_variable_get('@java_opts')
+
+    expect(java_opts).to include('-Djavax.net.ssl.trustStore=/home/vcap/app/.container_certificate_trust_store/truststore.jks')
+    expect(java_opts).to include('-Djavax.net.ssl.trustStorePassword=java-buildpack-trust-store-password')
+  end
+
+end
