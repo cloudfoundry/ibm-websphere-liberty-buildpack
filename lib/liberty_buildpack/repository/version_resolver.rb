62814638d39194e9b51cc91ab87ad6a1134c0c31
Nelson <nelson.alemar@upr.edu>
2017-09-20 15:24:28 -0400

    Error was that I was trying to find a number in an array using match method from string class

--- lib/liberty_buildpack/repository/version_resolver.rb
+++ lib/liberty_buildpack/repository/version_resolver.rb
@@ -51,8 +51,7 @@ module LibertyBuildpack
                     .select { |tokenized_version| matches? tokenized_candidate_version, tokenized_version }
                     .max { |a, b|
                       a.zip(b).each do |c, d|
-                        puts "Lo que ej: #{c}"
-                        if !/\A\d+\z/.match(a)
+                        if !/\A\d+\z/.match(c)
                           #Eliminating the letters (except ifx) for the string num in order to facilitate comparison
                           newNum = c.dup
                           if newNum.include? "ifx"
