b6bd9b762b63e7ab2a5d723024a2711ead075e55
Kevin Ortega <ortegake@us.ibm.com>
2018-12-21 15:46:10 -0500

    Update compile_spec.rb

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,7 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-B32HUX', 'USER_AGENT' => 'RSpec-Test')
+    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-B4WNHE', 'USER_AGENT' => 'RSpec-Test')
   end
 
   after(:each) do
