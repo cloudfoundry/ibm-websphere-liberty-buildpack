569d3ae7215cac8d13aacd9fc19c9e74ad98a140
Kevin Ortega <ortegake@us.ibm.com>
2019-01-24 10:38:29 -0500

    Update openjdk.rb

--- lib/liberty_buildpack/jre/openjdk.rb
+++ lib/liberty_buildpack/jre/openjdk.rb
@@ -110,8 +110,12 @@ module LibertyBuildpack::Jre
       FileUtils.mkdir_p(java_home)
 
       system "tar xzf #{file.path} -C #{java_home} --strip 1 2>&1"
-
-      if system("[ $(ls #{java_home} | wc -l) = 1 ]") && system("[ ! $(ls #{java_home} | grep -w 'jre') ]")
+      system "tar xzf #{file.path} -C #{java_home} --strip 1 2>&1"
+      rc=system("[ $(ls #{java_home} | wc -l) = 1 ]")
+      rc2=system("[ $(ls #{java_home} | grep -w notices.txt | wc -l) = 0 ]")
+      puts "files=#{files}"
+      
+      if rc && rc2
         FileUtils.rm_rf(java_home)
         FileUtils.mkdir_p(java_home)
         system "tar xzf #{file.path} -C #{java_home} --strip 2 2>&1"
