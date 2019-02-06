4e715ed3b019fd9f9474b78932f0aa2d0d5c5f0c
ortegake <ortegake@us.ibm.com>
2019-02-06 10:47:27 -0500

    update license code

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,7 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-B4WNHE', 'USER_AGENT' => 'RSpec-Test')
+    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-889R4V', 'USER_AGENT' => 'RSpec-Test')
   end
 
   after(:each) do
