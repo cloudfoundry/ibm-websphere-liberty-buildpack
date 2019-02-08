4176931a83144be1d2cfd1812f80cf5e71d3eb8a
Jhschube <36675747+Jhschube@users.noreply.github.com>
2019-02-08 10:49:18 -0500

    Update compose_mongo.rb

--- lib/liberty_buildpack/services/compose_mongo.rb
+++ lib/liberty_buildpack/services/compose_mongo.rb
@@ -62,7 +62,6 @@ module LibertyBuildpack::Services
       @db_name = "${#{db_var_name}}"
 
       @service_cert = credentials['ca_certificate_base64']
-      # raise "Resource #{@service_name} does not contain a uri property" if @service_cert.nil?
     end
 
     #------------------------------------------------------------------------------------
