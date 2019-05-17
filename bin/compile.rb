4dbb2be13de12a9d84aed290872f9948632a71b5
Preeti Sawant <Preeti.Sawant@ibm.com>
2019-05-17 12:59:59 -0400

    Update compile.rb

--- bin/compile.rb
+++ bin/compile.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # Encoding: utf-8
 # IBM WebSphere Application Server Liberty Buildpack
-# Copyright IBM Corp. 2017, 2019
+# Copyright IBM Corp. 2013, 2017
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
