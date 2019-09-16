3bb6956e2a18321d2c2a32c0b2514a1b8642653a
ortegake <ortegake@us.ibm.com>
2019-09-16 11:00:17 -0400

    updates for 19.0.0.9

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,7 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-BENESF', 'USER_AGENT' => 'RSpec-Test')
+    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-BEN4AJ', 'USER_AGENT' => 'RSpec-Test')
   end
 
   after(:each) do
