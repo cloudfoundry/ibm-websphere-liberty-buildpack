6353a7efd5473edc8e330e514fc0adc10e69a4e1
Preeti Sawant <preetisawant24@gmail.com>
2021-01-08 10:57:29 -0500

    debugging compression

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -473,8 +473,7 @@ module LibertyBuildpack::Container
         endpoint.add_attribute('host', '*')
       end
       endpoint.add_attribute('httpPort', "${#{KEY_HTTP_PORT}}")
-      if endpoints.include?('compression')
-      else
+      unless endpoints.include?('compression')
         endpoint.add_element('compression')
       end
       endpoint.delete_attribute('httpsPort')
