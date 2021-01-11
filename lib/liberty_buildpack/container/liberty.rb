e98a7ce8f3f40a22f4093d298bf8e827fe4c1102
ortegake <ortegake@us.ibm.com>
2021-01-11 12:06:28 -0500

    fix rubocop errors

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -647,14 +647,14 @@ module LibertyBuildpack::Container
         @services_manager.install_client_jars(@liberty_components_and_uris, current_server_dir)
       end
     end
-    
-    def populateClassCache
+
+    def populate_class_cache
       print "-----> Populating class cache \n"
       system "export PATH=$PATH:#{liberty_home}/../.java/jre/bin && #{liberty_home}/bin/server start defaultServer >null"
       system "export PATH=$PATH:#{liberty_home}/../.java/jre/bin && #{liberty_home}/bin/server stop defaultServer >null"
       system "export PATH=$PATH:#{liberty_home}/../.java/jre/bin && #{liberty_home}/bin/server start defaultServer >null"
       system "export PATH=$PATH:#{liberty_home}/../.java/jre/bin && #{liberty_home}/bin/server stop defaultServer >null"
-	  end
+    end
 
     # is the given liberty component required ? It may be non-optional, in which
     # case it is required, or it may be optional, in which case it is required
