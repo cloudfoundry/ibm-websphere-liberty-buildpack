39c14935b8f737ff12a73db747c43a6002631830
Raymond Xu <raymondxu92@gmail.com>
2021-04-19 08:59:13 -0400

    changed liberty license for 21.0.0.4

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,7 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-BYJHGS', 'USER_AGENT' => 'RSpec-Test')
+    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-BZCRWS', 'USER_AGENT' => 'RSpec-Test')
   end
 
   after(:each) do
