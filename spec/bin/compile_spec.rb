ce4e96e5a9ac6ac30f89b06f4fd3a0148d8c18d4
Raymond Xu <raymondxu92@gmail.com>
2022-01-18 12:08:29 -0500

    updated 22.0.0.1 license

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,7 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-C87LQV', 'USER_AGENT' => 'RSpec-Test')
+    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-CTUR-C9GKA5', 'USER_AGENT' => 'RSpec-Test')
   end
 
   after(:each) do
