15a31064ca47841455d2271b8614bd74255d63fe
Kevin Ortega <ortegake@us.ibm.com>
2023-02-20 12:42:40 -0500

    check for nil

--- lib/liberty_buildpack/jre/ibmjdk.rb
+++ lib/liberty_buildpack/jre/ibmjdk.rb
@@ -147,7 +147,7 @@ module LibertyBuildpack::Jre
     def self.find_ibmjdk(configuration)
       version, entry = LibertyBuildpack::Repository::ConfiguredItem.find_item(configuration)
       if entry.is_a?(Hash)
-        entry['license'] = entry['license'].sub('http', 'https') unless entry['license'].include? 'https'
+        entry['license'] = entry['license'].sub('http', 'https') unless (entry['license'].include? 'https') && !entry['license'].nil?
         return version, entry['uri'], entry['license']
       else
         return version, entry, nil
