a0fe3df6c42ec42e5fb478f498faa6e558218943
Kevin Ortega <ortegake@us.ibm.com>
2024-04-24 12:44:19 -0400

    Updates for WLP 24.0.0.4

--- spec/bin/compile_spec.rb
+++ spec/bin/compile_spec.rb
@@ -26,7 +26,7 @@ describe 'compile script', :integration do
   end
 
   before(:each) do
-    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-KZJT-27HQXL', 'USER_AGENT' => 'RSpec-Test')
+    ENV.update('IBM_JVM_LICENSE' => 'L-SMKR-AVSEUH', 'IBM_LIBERTY_LICENSE' => 'L-MFLD-UE7SJJ', 'USER_AGENT' => 'RSpec-Test')
   end
 
   after(:each) do
